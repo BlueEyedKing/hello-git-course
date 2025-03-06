d28e940c56a0741f64567a7a4602f431cb4544ac
Niklas Kasakkamäki <niklas.kasakkamaki@tuni.fi>
2025-03-06 13:45:14 +0200

    Fix merge conflicts

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,3 @@
 print("Hello world!")
-<<<<<<< HEAD
-print("Hello once again")
-=======
+print("Hello darkness my old friend")
 print("I've come to talk with you again")
->>>>>>> upstream2/feature/modify-print
